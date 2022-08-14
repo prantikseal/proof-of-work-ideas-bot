647030dcbd8b4cf4a597aee898918eb2062ccae0
prantikseal <83081442+prantikseal@users.noreply.github.com>
2022-08-14 21:29:26 +0530

    added test bot

--- src/index.js
+++ src/index.js
@@ -85,33 +85,53 @@ client.on('messageCreate', (message) => {
 })
 
 client.on('interactionCreate', (interaction) => {
-    if (interaction.options.getString('type')=== 'development') {
+    if (interaction.options.getString('category')=== 'development') {
 
-        interaction.reply({content: `You asked for a proof of work idea in ${interaction.options.getString('type')}`+ '\n **Here is a proof of work idea for you in Development :)**\n\n'+ randomDev()});
+        interaction.reply({content: `You asked for a proof of work idea in ${interaction.options.getString('category')}`+ '\n **Here is a proof of work idea for you in Development :)**\n\n'+ randomDev()});
 
     }
-    else if (interaction.options.getString('type')=== 'design') {
-            
-            interaction.reply({content: `You asked for a proof of work idea in ${interaction.options.getString('type')}`+ '\n **Here is a proof of work idea for you in Design :)**\n\n'+ randomDesign()});
+    else if (interaction.options.getString('category')=== 'design') {
+
+            interaction.reply({content: `You asked for a proof of work idea in ${interaction.options.getString('category')}`+ '\n **Here is a proof of work idea for you in Design :)**\n\n'+ randomDesign()});
     
         }
-    else if (interaction.options.getString('type')=== 'copywriting') {
-        interaction.reply({content: `You asked for a proof of work idea in ${interaction.options.getString('type')}`+ '\n **Here is a proof of work idea for you in Copywriting :)**\n\n'+ randomCopy()});
+    else if (interaction.options.getString('category')=== 'copywriting') {
+        interaction.reply({content: `You asked for a proof of work idea in ${interaction.options.getString('category')}`+ '\n **Here is a proof of work idea for you in Copywriting :)**\n\n'+ randomCopy()});
     }
-    else if (interaction.options.getString('type')=== 'marketing') {
-        interaction.reply({content: `You asked for a proof of work idea in ${interaction.options.getString('type')}`+ '\n **Here is a proof of work idea for you in Marketing :)**\n\n'+ randomMarketing()});
+    else if (interaction.options.getString('category')=== 'marketing') {
+        interaction.reply({content: `You asked for a proof of work idea in ${interaction.options.getString('category')}`+ '\n **Here is a proof of work idea for you in Marketing :)**\n\n'+ randomMarketing()});
     }
-    else if (interaction.options.getString('type')=== 'community') {
-        interaction.reply({content: `You asked for a proof of work idea in ${interaction.options.getString('type')}`+ '\n **Here is a proof of work idea for you in Community :)**\n\n'+ randomCommunity()});
+    else if (interaction.options.getString('category')=== 'community') {
+        interaction.reply({content: `You asked for a proof of work idea in ${interaction.options.getString('category')}`+ '\n **Here is a proof of work idea for you in Community :)**\n\n'+ randomCommunity()});
     }
-    else if (interaction.options.getString('type')=== 'writing') {
-        interaction.reply({content: `You asked for a proof of work idea in ${interaction.options.getString('type')}`+ '\n **Here is a proof of work idea for you in Writing :)**\n\n'+ randomWriting()});
+    else if (interaction.options.getString('category')=== 'writing') {
+        interaction.reply({content: `You asked for a proof of work idea in ${interaction.options.getString('category')}`+ '\n **Here is a proof of work idea for you in Writing :)**\n\n'+ randomWriting()});
     }
-    else if (interaction.options.getString('type')=== 'all') {
-        interaction.reply({content: `You asked for a proof of work idea in ${interaction.options.getString('type')}`+ '\n **Here is a proof of work idea for you in all domains :)**\n\n'+ "***In Development \n***"+randomDev()+'\n\n'+ "***In Design \n***"+ randomDesign()+'\n\n'+ "***In Copywriting \n***"+randomCopy()+'\n\n'+"***In Marketing \n***"+ randomMarketing()+'\n\n'+"***In Community \n***"+ randomCommunity()+'\n\n'+"***In Writing \n***"+ randomWriting()});
+    else if (interaction.options.getString('category')=== 'all') {
+        interaction.reply({content: `You asked for a proof of work idea in ${interaction.options.getString('category')}`+ '\n **Here is a proof of work idea for you in all domains :)**\n\n'+ "***In Development \n***"+randomDev()+'\n\n'+ "***In Design \n***"+ randomDesign()+'\n\n'+ "***In Copywriting \n***"+randomCopy()+'\n\n'+"***In Marketing \n***"+ randomMarketing()+'\n\n'+"***In Community \n***"+ randomCommunity()+'\n\n'+"***In Writing \n***"+ randomWriting()});
     }
-    else if(interaction.options.getString('type')=== 'help'){
-        interaction.reply({content: `This bot was made with love from https://fueler.io/`+ '\n **Here is a list of commands you can use**\n\n'+ "***- development ***\n"+ "***- design ***\n"+ "***- copywriting ***\n"+ "***- marketing ***\n"+ "***- community ***\n"+ "***- writing ***\n"+ "***- all ***\n"});
+    else if(interaction.options.getString('category')=== 'help'){
+        interaction.reply({
+            "content": ":book: *GETTING STARTED? READ THE QUICK START GUIDE*",
+            "embeds": [
+              {
+                "title": ":sparkle: *BASIC COMMANDS*",
+                "description": "`/proof-of-work`  Slash Command to Access \n       `category:`  Shows the category of proof of work you can access for the time being.\nIn short, `/proof-of-work <Category> `\n\nFor a full list of Ideas visit <https://fueler.io/proof-of-work-ideas>",
+                "color": 65535
+              },
+              {
+                "title": ":white_check_mark: *OPTIONS IN CATEGORY:*",
+                "description": "Parameters are options added to the end of your prompt that change which Idea you get.\n       - *Development:* Will get you ideas around Development\n       - *Design:*  Will get you ideas around Design.\n       - *Copywriting:*  Will get you ideas around CopyWriting.\n       - *Marketing:*  Will get you ideas around Marketing.\n       - *Community:*  Will get you ideas around Community Management.\n       - *All:* Bored? And don't know what to create choose `all` and get any ideas to Implement on :wink:",
+                "color": 65535
+              },
+              {
+                "title": ":man_technologist: *WEBSITE*",
+                "description": "View all the proof of work and access your account on \n<https://fueler.io/proof-of-work-ideas>",
+                "color": 65535
+              }
+            ],
+            "attachments": []
+          });
     }
 })
 
@@ -122,8 +142,8 @@ async function main() {
             description: 'Command to get new proof of work ideas in the chat',
             options : [
                 {
-                    name: 'type',
-                    description: 'Get proof of work ideas for a specific type',
+                    name: 'category',
+                    description: 'Get proof of work ideas for a specific category',
                     type: 3,
                     required: true,
                     "choices": [
